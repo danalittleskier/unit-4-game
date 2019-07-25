880844aa4d1fb923e757c15d5a273a6da45828d1
DanaLittleSkier <dana.alexandrescu@gmail.com>
2019-07-25 14:29:25 -0600

    Adding random number function on page reload

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,9 @@
-// Create a string which will hold the lottery number
-var lottoNumber = "";
 
-// Then initiate a loop to generate 9 separate numbers
-for (var i = 0; i < 9; i++) {
 
-  // For each iteration, generate a new random number between 0 and 9.
-  var random = Math.floor(Math.random() * 10);
-
-  // Take this number and then add it to the rest of the string.
-  // In essence, we are iteratively building a string of numbers. (e.g. First: 1, Second: 13, Third: 135, etc.)
-  lottoNumber = random + lottoNumber;
-
-}
+$( document ).ready(function() {
+ // For each iteration, generate a new random number between 19 and 120.
+    var random = Math.floor(Math.random() * 120 ) + 19;
+    console.log("Ready!")
+  $("#random-number").text(random);
+});
+ 
