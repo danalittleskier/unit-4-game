3fbaeeb7c13018b7d4b06716514721728d765039
DanaLittleSkier <dana.alexandrescu@gmail.com>
2019-07-25 20:26:21 -0600

    Changed style sheet with bigger crystal boxes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,8 +2,11 @@
 
 $( document ).ready(function() {
  // For each iteration, generate a new random number between 19 and 120.
-    var random = Math.floor(Math.random() * 120 ) + 19;
+    var random = getRandomNumber(19, 120);
     console.log("Ready!")
   $("#random-number").text(random);
 });
  
+function getRandomNumber(min, max){
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
