efd38c112fb8c89b36c89631a8d03e3a17b463e6
DanaLittleSkier <dana.alexandrescu@gmail.com>
2019-07-25 20:54:35 -0600

    Created an object and properties for the game.  Connected variables with DOM

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,30 @@
-
+var crystalgame = {
+    score : 0,
+    wins : 0,
+    losses : 0,
+    randomWinningNum : "",
+    randomCrystal1Num : "",
+    randomCrystal2Num : "",
+    randomCrystal3Num : "",
+    randomCrystal4Num : ""
+}
 
 $( document ).ready(function() {
  // For each iteration, generate a new random number between 19 and 120.
-    var random = getRandomNumber(19, 120);
-    console.log("Ready!")
-  $("#random-number").text(random);
+    crystalgame.randomWinningNum = getRandomNumber(19, 120);
+    crystalgame.randomCrystal1Num = getRandomNumber(1, 12);
+    crystalgame.randomCrystal2Num = getRandomNumber(1, 12);
+    crystalgame.randomCrystal3Num = getRandomNumber(1, 12);
+    crystalgame.randomCrystal4Num = getRandomNumber(1, 12);
+    
+    $("#random-number").text(crystalgame.randomWinningNum);
+    $("#crystal-1").text(crystalgame.randomCrystal1Num);
+    $("#crystal-2").text(crystalgame.randomCrystal2Num);
+    $("#crystal-3").text(crystalgame.randomCrystal3Num);
+    $("#crystal-4").text(crystalgame.randomCrystal4Num);
+    $("#wins").text(crystalgame.wins);
+    $("#losses").text(crystalgame.losses);
+
 });
  
 function getRandomNumber(min, max){
