9a655d29a77e75698e2c9b00d1ba7d43a13afcc2
DanaLittleSkier <dana.alexandrescu@gmail.com>
2019-07-27 19:03:55 -0600

    Creating on click function for the crystals.  Outputing result to html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,5 @@
+var counter = 0;
+
 var crystalgame = {
     score : 0,
     wins : 0,
@@ -6,16 +8,19 @@ var crystalgame = {
     randomCrystal1Num : "",
     randomCrystal2Num : "",
     randomCrystal3Num : "",
-    randomCrystal4Num : ""
+    randomCrystal4Num : "",
+    createNewGame : function(){
+        crystalgame.randomWinningNum = getRandomNumber(19, 120);
+        crystalgame.randomCrystal1Num = getRandomNumber(1, 12);
+        crystalgame.randomCrystal2Num = getRandomNumber(1, 12);
+        crystalgame.randomCrystal3Num = getRandomNumber(1, 12);
+        crystalgame.randomCrystal4Num = getRandomNumber(1, 12);
+    }
 }
 
 $( document ).ready(function() {
  // For each iteration, generate a new random number between 19 and 120.
-    crystalgame.randomWinningNum = getRandomNumber(19, 120);
-    crystalgame.randomCrystal1Num = getRandomNumber(1, 12);
-    crystalgame.randomCrystal2Num = getRandomNumber(1, 12);
-    crystalgame.randomCrystal3Num = getRandomNumber(1, 12);
-    crystalgame.randomCrystal4Num = getRandomNumber(1, 12);
+    crystalgame.createNewGame();
     
     $("#random-number").text(crystalgame.randomWinningNum);
     $("#crystal-1").text(crystalgame.randomCrystal1Num);
@@ -25,8 +30,36 @@ $( document ).ready(function() {
     $("#wins").text(crystalgame.wins);
     $("#losses").text(crystalgame.losses);
 
+    $("#crystal-1").attr("data-crystalvalue", crystalgame.randomCrystal1Num);
+    $("#crystal-2").attr("data-crystalvalue", crystalgame.randomCrystal2Num);
+    $("#crystal-3").attr("data-crystalvalue", crystalgame.randomCrystal3Num);
+    $("#crystal-4").attr("data-crystalvalue", crystalgame.randomCrystal4Num);   
+
 });
  
 function getRandomNumber(min, max){
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
+
+function addToNumber(first, second){
+    return first + second;
+}
+
+$(".crystal-image").on("click", function() {
+
+    var crystalValue = ($(this).attr("data-crystalvalue"));
+    crystalValue = parseInt(crystalValue);
+
+    counter += crystalValue;
+
+    $("#score").text(counter);
+
+    if(counter === crystalgame.randomWinningNum){
+        $("#finish-game").text("You Won!");
+        crystalgame.createNewGame();
+
+    } else if (counter > crystalgame.randomWinningNum){
+        $("#finish-game").text("You Lost!");
+        crystalgame.createNewGame();
+    }
+});
